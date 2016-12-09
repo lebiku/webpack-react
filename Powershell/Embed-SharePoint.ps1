a7b8806ee461bd18b58eb19dab505f7b8812a169
Markus Wälchli <mkw@nexplore.ch>
2016-12-09 16:43:23 +0100

    PS Script Updated

--- Powershell/Embed-SharePoint.ps1
+++ Powershell/Embed-SharePoint.ps1
@@ -4,9 +4,16 @@
 #Install-Module SharePointPnPPowerShell2016
 Install-Module SharePointPnPPowerShell2013
 
-
 $siteUrl = "http://kapo.nexplore.ch/sites/app"
 Connect-SPOnline -Url $siteUrl -Credentials (Get-Credential)
+
+$layout = [OfficeDevPnP.Core.WikiPageLayout]::TwoColumns;
+
+Add-PnPWikiPage -Layout $layout -ServerRelativePageUrl '/sites/app/SitePages/apploader.aspx';        
+Add-PnPWebPartToWikiPage -ServerRelativePageUrl '/sites/app/SitePages/apploader.aspx' -Path '.\AppLoader.webpart' -Row 1 -Column 1
+
+
+
 
 
 ### inject javascript
